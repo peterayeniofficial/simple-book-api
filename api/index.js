246d78ba6f3973c5f216e97e1cf5b3ec3366b84b
Peter Ayeni <peterayeni@Peters-MacBook-Pro.local>
2019-07-08 15:12:41 +0100

    Add routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
+import config from 'dotenv'
 import express from 'express';
 import bodyParser from 'body-parser'
+import bookRoutes from './server/routes/BookRoutes'
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 const port = process.env.PORT || 8000;
+
+app.use('/api/v1/books', bookRoutes)
 
 // when a random route is inputed
 app.get('*', (req,res) => res.status(200).send({
